87b341cc0851d63de9a8075cf567ea49b3c275c8
DillonSummers <dsumm11@lsu.edu>
2025-02-10 17:05:32 -0600

    Dillon

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Huarong Teng was here'),
+      home: const MyHomePage(title: 'Huarong Teng was here Dillon was here'),
     );
   }
 }
